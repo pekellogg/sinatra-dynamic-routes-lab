c6e85272559eb4b89c75de0457d6a15edfacc9e4
Peyton <pekellogg@hotmail.com>
2022-03-14 14:17:30 -0700

    complete

--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    erb :reversename
+  end
+
+  get "/square/:number" do
+    erb :squarenumber
+  end
+  
+  get "/say/:number/:phrase" do
+    erb :numberphrase
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    erb :manywords
+  end
+
+  get "/:operation/:number1/:number2" do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    case params[:operation]
+    when "add"
+        (num1 + num2).to_s
+    when "subtract"
+        (num1 - num2).to_s
+    when "multiply"
+        (num1 * num2).to_s
+    when "divide"
+        (num1 / num2).to_s
+    end
+  end
 
 end
